64078ead836eb2ca61b339e5ef9b7e72e8d26359
Vouu <contact@mtz.pw>
2023-11-22 17:11:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ for (const file of servapps) {
     if (fs.existsSync(`./servapps/${file}/artefacts`)) {
         const artefacts = fs.readdirSync(`./servapps/${file}/artefacts`)
         for (const artefact of artefacts) {
-            servapp.artefacts[artefact] = (`https://comos.manhtuong.net/servapps/${file}/artefacts/${artefact}`)
+            servapp.artefacts[artefact] = (`https://cosmos.manhtuong.net/servapps/${file}/artefacts/${artefact}`)
         }
     }
 
